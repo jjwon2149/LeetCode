c1ecb44a309446ba6070a63ebfb5ce8c843e87fe
jjwon2149 <jjwon2149@gmail.com>
2024-12-05 15:07:35 +0900

    2337. Move Pieces to Obtain a String

---
+++ LeetCode/LeetCode/MovePiecetoObtainaString.swift
@@ -0,0 +1,40 @@
+//
+//  MovePiecetoObtainaString.swift
+//  LeetCode
+//
+//  Created by 정종원 on 12/5/24.
+//
+
+import Foundation
+
+class MovePiecetoObtainaString {
+    func canChange(_ start: String, _ target: String) -> Bool {
+        
+        let checkStart = start.replacingOccurrences(of: "[_]", with: "", options: .regularExpression)
+        let checkTarget = target.replacingOccurrences(of: "[_]", with: "", options: .regularExpression)
+        if checkStart != checkTarget {
+            return false
+        }
+        
+        let startPosition = start.enumerated().filter{ $0.element != "_"} //L의 Index와 Value
+        let targetPosition = target.enumerated().filter{ $0.element != "_"} // R의 Index와 Value
+        
+        if startPosition.map({ $0.element }) != targetPosition.map({ $0.element }) {
+            return false
+        }
+        
+        for (startTuple, targetTuple) in zip(startPosition, targetPosition) {
+            let (startIndex, char) = startTuple
+            let (targetIndex, _) = targetTuple
+            
+            if char == "L" && startIndex < targetIndex {
+                return false
+            }
+            if char == "R" && startIndex > targetIndex {
+                return false
+            }
+        }
+        
+        return true
+    }
+}
